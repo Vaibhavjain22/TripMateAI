0862e449b54cae4db065fbe29c9f41096263d5ce
vaibhavjain76847 <vaibhavjain76847@gmail.com>
2026-09-09 22:18:40 +0530

    graph built and agent defined

--- backend.py
+++ backend.py
@@ -189,3 +189,11 @@ graph.add_node("hotel_agent", hotel_agent)
 graph.add_node("itinerary_agent", itinerary_agent)
 graph.add_node("final_agent", final_agent)
 
+
+graph.add_edge(START,"flight_agent" )
+graph.add_edge("flight_agent", "hotel_agent")
+graph.add_edge("hotel_agent","itinerary_agent")
+graph.add_edge("itinerary_agent", "final_agent")
+graph.add_edge("final_agent", END)
+
+
