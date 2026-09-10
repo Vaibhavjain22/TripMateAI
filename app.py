e41ac24a04a4914f5bb950c2ab3f297af6332ac4
vaibhavjain76847 <vaibhavjain76847@gmail.com>
2026-09-10 21:48:19 +0530

    feat: all the lib imported

---
+++ app.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import traceback
+import uvicorn
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+from backend import run_travel_agent
